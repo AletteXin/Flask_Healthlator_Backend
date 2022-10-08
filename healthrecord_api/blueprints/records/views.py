c7d4224a7458192ffab797141417a16d3d8cab75
AletteXin <alettengxinling@gmail.com>
2022-10-07 23:51:49 -0400

    new model

--- healthrecord_api/blueprints/records/views.py
+++ healthrecord_api/blueprints/records/views.py
@@ -33,20 +33,18 @@ def create():
     current_time = datetime.datetime.now
     now = print(current_time)
 
-    print(new_entry['Asthma'])
-    print(new_entry['Livercancer'])
-
     create = Form.create(Name = new_entry['Name'], UserID = new_entry['ID'], Gender = new_entry['Gender'], Birthdate = new_entry['Birthdate'], 
     Address = new_entry['Address'], Medications = new_entry['Medications'], Nameofnextkin = new_entry['Nameofnextkin'],
     Phoneofnextkin = new_entry['Phoneofnextkin'], Reasonforvisit = new_entry['Reasonforvisit'], Fever = new_entry['Fever'], 
     Headache = new_entry['Headache'], Nightchills = new_entry['Nightchills'], Sorethroat = new_entry['Sorethroat'], Cough = new_entry['Cough'], 
     Breathingdiff = new_entry['Breathingdiff'], Diarrhoea = new_entry['Diarrhoea'], Chestpain = new_entry['Chestpain'], 
     Legnumbness = new_entry['Legnumbness'], Handnumbness = new_entry['Handnumbness'], Facenumbness = new_entry['Facenumbness'], 
-    Abdominalpain =  new_entry['Abdominalpain'], Diabetes = new_entry['Diabetes'], Highbloodpressure = new_entry['Highbloodpressure'], 
+    Abdominalpain =  new_entry['Abdominalpain'], Vomit = new_entry['Vomit'], Dizziness = new_entry['Dizziness'], 
+    Sleepingdiff = new_entry['Sleepingdiff'], Diabetes = new_entry['Diabetes'], Highbloodpressure = new_entry['Highbloodpressure'], 
     Highcholesterol = new_entry['Highcholesterol'], Asthma = new_entry['Asthma'], Kidneydisease = new_entry['Kidneydisease'],
-    Arthritis = new_entry['Arthritis'], Pancreaticcancer = new_entry['Pancreaticcancer'], Livercancer = new_entry['Livercancer'], 
-    Colorectalcancer = new_entry['Colorectalcancer'], COPD = new_entry['COPD'], Depression = new_entry['Depression'], 
-    Lungcancer = new_entry['Lungcancer'])
+    Arthritis = new_entry['Arthritis'], Cancer = new_entry['Cancer'], Liverdisease = new_entry['Liverdisease'], 
+    Stroke = new_entry['Stroke'], COPD = new_entry['COPD'], Depression = new_entry['Depression'], 
+    Alzheimer = new_entry['Alzheimer'])
 
 
     message = []
@@ -86,12 +84,13 @@ def show():
         'Sorethroat': record_exists.Sorethroat, 'Cough': record_exists.Cough, 'Breathingdiff': record_exists.Breathingdiff,
         'Diarrhoea': record_exists.Diarrhoea, 'Chestpain': record_exists.Chestpain, 'Legnumbness': record_exists.Legnumbness,
         'Handnumbness': record_exists.Handnumbness, 'Facenumbness': record_exists.Facenumbness, 
-        'Abdominalpain': record_exists.Abdominalpain, 'Diabetes': record_exists.Diabetes,
+        'Abdominalpain': record_exists.Abdominalpain, 'Vomit': record_exists.Vomit, 'Dizziness': record_exists.Dizziness, 
+        'Sleepingdiff': record_exists.Sleepingdiff, 'Diabetes': record_exists.Diabetes,
         'Highbloodpressure': record_exists.Highbloodpressure, 'Highcholesterol': record_exists.Highcholesterol, 
         'Asthma': record_exists.Asthma, 'Kidneydisease': record_exists.Kidneydisease, 'Arthritis': record_exists.Arthritis,
-        'Pancreaticcancer': record_exists.Pancreaticcancer, 'Livercancer': record_exists.Livercancer, 
-        'Colorectalcancer': record_exists.Colorectalcancer, 'COPD': record_exists.COPD, 'Depression': record_exists.Depression, 
-        'Lungcancer': record_exists.Lungcancer
+        'Cancer': record_exists.Cancer, 'Liverdisease': record_exists.Liverdisease, 
+        'Stroke': record_exists.Stroke, 'COPD': record_exists.COPD, 'Depression': record_exists.Depression, 
+        'Alzheimer': record_exists.Alzheimer
          }])
 
     
@@ -105,11 +104,12 @@ def show():
         'Sorethroat': "", 'Cough': "", 'Breathingdiff': "",
         'Diarrhoea': "", 'Chestpain': "", 'Legnumbness': "",
         'Handnumbness': "", 'Facenumbness': "", 
-        'Abdominalpain': "", 'Diabetes': "",
+        'Abdominalpain': "", 'Vomit': "", 'Dizziness': "", 
+        'Sleepingdiff': "",'Diabetes': "",
         'Highbloodpressure': "", 'Highcholesterol': "", 
         'Asthma': "", 'Kidneydisease': "", 'Arthritis': "",
-        'Pancreaticcancer': "", 'Livercancer': "", 
-        'Colorectalcancer': "", 'COPD': "", 'Depression': "", 'Lungcancer': ""}])
+        'Cancer': "", 'Liverdisease': "", 
+        'Stroke': "", 'COPD': "", 'Depression': "", 'Alzheimer': ""}])
 
     print(record_details)
 
